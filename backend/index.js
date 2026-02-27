30449958e1bbe13b84d4a6d710ba8bd46dab4b49
Jossoey <jossoey84@gmail.com>
2026-02-27 17:25:08 +0700

    Install Express

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
   res.send("Server running");
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
